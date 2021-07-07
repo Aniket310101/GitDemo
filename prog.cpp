0d4788cd4ed38319225623a0ae836f0fdbdef2ac
Aniket <aniketpurkayastha722@gmail.com>
2021-07-07 23:55:55 +0530

    changed develop 1

--- prog.cpp
+++ prog.cpp
@@ -8,9 +8,11 @@ int main(){
 
     int n=10;
     int cnt = 0;
+    int c = 1;
     for(int i = 0;i<n;i++){
         cnt+=i;
+        c*=i;
     }
-    cout<<cnt<<endl;
+    cout<<cnt<<endl<<c;
 
 }
