f3b0c9654d1e75205f55c92b3fc229735954754f
Aniket <aniketpurkayastha722@gmail.com>
2021-07-07 23:54:09 +0530

    initial commit

---
+++ prog.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n=10;
+    int cnt = 0;
+    for(int i = 0;i<n;i++){
+        cnt+=i;
+    }
+    cout<<cnt<<endl;
+
+}
